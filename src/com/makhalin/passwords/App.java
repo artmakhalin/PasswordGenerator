716c751aef62017f44e6b760a383d987a8c9fc0e
artmakhalin <artmakhalin@gmail.com>
2025-03-11 13:27:16 -0400

    string generation

--- src/com/makhalin/passwords/App.java
+++ src/com/makhalin/passwords/App.java
@@ -1,4 +1,8 @@
 package com.makhalin.passwords;
 
 public class App {
+    public static void main(String[] args) {
+        PasswordGenerator gen = new PasswordGenerator();
+        System.out.println(gen.generatePassword(10, true, true, true, true));
+    }
 }
