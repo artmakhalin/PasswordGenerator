405dc7d08f2551aa5de09d4d70c60afcb55fb5cc
artmakhalin <artmakhalin@gmail.com>
2025-03-12 21:27:55 -0400

    completed

--- src/com/makhalin/passwords/App.java
+++ src/com/makhalin/passwords/App.java
@@ -2,7 +2,16 @@ package com.makhalin.passwords;
 
 public class App {
     public static void main(String[] args) {
+        UserInterface ui = new TextBasedInterface();
+
+        int length = ui.getPasswordLength();
+        boolean userLowercase = ui.useLowercase();
+        boolean userUppercase = ui.useUppercase();
+        boolean useNumbers = ui.useNumbers();
+        boolean useSymbols = ui.useSymbols();
+
         PasswordGenerator gen = new PasswordGenerator();
-        System.out.println(gen.generatePassword(10, true, true, true, true));
+        String password = gen.generatePassword(length, userLowercase, userUppercase, useNumbers, useSymbols);
+        ui.displayPassword(password);
     }
 }
